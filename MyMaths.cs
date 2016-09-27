7b4b7a2e856acb933bb6dc8bf7dd6bccaac5dbc8
lagoyu <lagoyu@users.noreply.github.com>
2016-09-27 21:10:49 +0100

    First Project Commit

---
+++ MyMaths.cs
@@ -0,0 +1,18 @@
+﻿namespace FibonacciWithMyMathsClass
+{
+    public class MyMaths
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciWithMyMathsClass
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("{0} {1}", i, MyMaths.Fibonacci(i));
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
